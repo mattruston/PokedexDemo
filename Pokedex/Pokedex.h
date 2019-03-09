da40d5d665fe92b75f071341dd660bd5ec57af1c
Matt Ruston <mruston@underarmour.com>
2019-03-09 10:42:03 -0600

    Initial Commit

---
+++ Pokedex/Pokedex.h
@@ -0,0 +1,19 @@
+//
+//  Pokedex.h
+//  Pokedex
+//
+//  Created by Matthew Ruston on 3/9/19.
+//  Copyright © 2019 Under Armour, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pokedex.
+FOUNDATION_EXPORT double PokedexVersionNumber;
+
+//! Project version string for Pokedex.
+FOUNDATION_EXPORT const unsigned char PokedexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pokedex/PublicHeader.h>
+
+
